e8af296ba3c5227701967096d257153fa18c50bc
zhoudad <aczhouxiao@gmail.com>
2022-05-13 18:11:38 +0800

    feat:测试

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              '点击按钮次数:',
             ),
             Text(
               '$_counter',
